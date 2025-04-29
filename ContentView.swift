cfbc4816a1f326d2e6575b60350774b1472e9989
Tsogtuguldur <terdenebulgan6508@stu.d214.org>
2025-04-29 14:30:38 -0500

    added number3

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        @State var number3 = ""
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Spacer()
+            
+         
+           
         }
     }
 }
